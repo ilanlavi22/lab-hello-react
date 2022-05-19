64341adaec2c0ee6153ab6919a923428a3698109
ilanlavi <refaelberlin@gmail.com>
2022-05-19 17:54:36 +0200

    Lab - Hello React -  Done

--- src/Content.js
+++ src/Content.js
@@ -9,26 +9,26 @@ const Content = () => {
       <div className="react-features">
         <div className="feature">
           <img src={icon1} alt="" />
-          <h3>a</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nulla!</p>
+          <h3>Declarative</h3>
+          <p>React makes it painless to create interactive UIs.</p>
         </div>
 
         <div className="feature">
           <img src={icon2} alt="" />
-          <h3>c</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nulla!</p>
+          <h3>Component-Based</h3>
+          <p>Build encapsulated components that manage their own state, then compose them to make complex UIs.</p>
         </div>
 
         <div className="feature">
           <img src={icon3} alt="" />
-          <h3>b</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nulla!</p>
+          <h3>Single-Way</h3>
+          <p>We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.</p>
         </div>
 
         <div className="feature">
           <img src={icon4} alt="" />
-          <h3>a</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nulla!</p>
+          <h3>Introducing JSX</h3>
+          <p>This funny tag syntax is neither a string nor HTML. It is called JSX, and it is a syntax extension to JavaScript.</p>
         </div>
 
       </div>
